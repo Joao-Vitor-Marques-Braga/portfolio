e8b63ff476ba461b32d3e8cc86c9eb350235bd4c
João Vitor Marques Braga <102833436+Joao-Vitor-Marques-Braga@users.noreply.github.com>
2023-01-11 11:29:00 -0300

    Add files via upload

--- src/components/Meus Projetos/index.jsx
+++ src/components/Meus Projetos/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './style.css'
 import crud from '../../images/images-projects/crud.png'
 import cep from  '../../images/images-projects/buscar-cep.png'
+import form from '../../images/images-projects/forms.png'
 
 export const MeusProjetos = () => {
   return (
@@ -9,7 +10,7 @@ export const MeusProjetos = () => {
       <div className='sistema-crud'>   
         <h1>Sistema CRUD</h1>
         <a href="https://joao-vitor-marques-braga.github.io/sistema-CRUD-JavaScript/">
-          <img src={crud} width='75%' alt="" />
+          <img src={crud} width='75%'/>
         </a>
         <h3>Desenvolvido com HTML, CSS e JavaScript</h3>
       </div>
@@ -17,9 +18,16 @@ export const MeusProjetos = () => {
       <div>
         <h1>Rastrear CEP</h1>
         <a href="https://rastrear-cep.vercel.app/">
-          <img src={cep} width='75%' alt="" />
+          <img src={cep} width='75%'/>
         </a>
           <h3>Desenvolvido com o framework React.JS e a utilizaçãop da API 'viacep'</h3>
+      </div>
+      
+      <div>
+        <h1>Formulário Sobre Desenvolvedores</h1>
+        <a href="https://formulario-dev-pearl.vercel.app/">
+          <img src={form} width='75%'/>
+        </a>
       </div>
     </div>
   )
--- src/components/Minhas habilidades/index.jsx
+++ src/components/Minhas habilidades/index.jsx
@@ -10,35 +10,35 @@ export const MinhasHabilidades = () => {
         <div className='habilidades'>
           <div className='card-html'>
             <img src="https://img.icons8.com/color/240/null/html-5--v1.png"/>
-            <h4>Nível: Intermediário</h4>
+            
           </div>
           <div className='card-css'>
             <img src="https://img.icons8.com/color/240/null/css3.png"/>
-            <h4>Nível: Intermediário</h4>
+            
           </div>
           <div className='card-javascript'>
             <img src="https://img.icons8.com/color/240/null/javascript--v1.png"/>
-            <h4>Nível: Básico</h4>
+            
           </div>
           <div className='card-react'>
             <img src="https://img.icons8.com/plasticine/240/null/react.png"/>
-            <h4>Nível: Básico</h4>
+            
           </div>
           <div className='card-bootstrap'>
             <img src="https://img.icons8.com/color/240/null/bootstrap.png"/>
-            <h4>Nível: Básico</h4>
+            
           </div>
           <div className='card-python'>
             <img src="https://img.icons8.com/color/240/null/python--v1.png"/>
-            <h4>Nível: Intermediário</h4>
+           
           </div>
           <div className='card-flask'>
             <img src="https://img.icons8.com/fluency/240/000000/flask.png"/>
-            <h4>Nível: Básico</h4>
+            
           </div>
           <div className='card-mysql'>
             <img src="https://img.icons8.com/color/240/null/mysql-logo.png"/>
-            <h4>Nível: Básico</h4>
+            
           </div>
         </div>
     </div>
