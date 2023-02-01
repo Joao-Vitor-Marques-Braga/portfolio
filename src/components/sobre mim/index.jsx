278d7c560855b0ab0a962980a21720eea1fd2d32
João Vitor Marques Braga <102833436+Joao-Vitor-Marques-Braga@users.noreply.github.com>
2023-02-01 12:27:21 -0300

    Update index.jsx

--- src/components/sobre mim/index.jsx
+++ src/components/sobre mim/index.jsx
@@ -13,7 +13,7 @@ export const SobreMim = () => {
             <h3>
               Tenho 17 anos, iniciante no mundo do desenvolvimento buscando uma grande carreira,
               atualmente cursando Engenharia de Software (Bacharelado) pela Universidade de Rio Verde (UNIRV).
-              Ensino médio concluido na Unidade Integrada SESI SENAI Rio Verde, além do curso técnico em desenvolvimento de sistemas.
+              Ensino médio concluído na Unidade Integrada SESI SENAI Rio Verde, além do curso técnico em desenvolvimento de sistemas.
               Possuo experiencia na area de front-end e back-end além de conhecimento sobre o gerenciamento de projetos.
             </h3>
           </div>
